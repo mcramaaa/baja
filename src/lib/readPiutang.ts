3e65a1e15a729ec01285c38e40905190e2e46b7d
mcramaaa <mchrama28@gmail.com>
2025-02-04 16:41:32 +0700

    finish piutang

--- src/lib/readPiutang.ts
+++ src/lib/readPiutang.ts
@@ -3,7 +3,7 @@ import { IPiutang } from "@/interface/IPiutang";
 import { google } from "googleapis";
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: "./.key.json",
+  keyFile: "./key.json",
   scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
 });
 
